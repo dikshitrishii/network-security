f65abfe72205869c64a2fd96455795316999de3a
Dikshit Rishi <rishidikshit987@gmail.com>
2025-08-12 19:04:51 +0530

    added the etl pipeline

--- networksecurity/logging/logger.py
+++ networksecurity/logging/logger.py
@@ -2,8 +2,6 @@ import logging
 import os
 from datetime import datetime
 
-logger = logging.getLogger(__name__)
-
 LOG_FILE = f"{datetime.now().strftime('%m-%d-%Y %H:%M:%S')}.log"
 
 logs_path = os.path.join(os.getcwd(), "logs")
@@ -14,9 +12,5 @@ log_file_path = os.path.join(logs_path, LOG_FILE)
 logging.basicConfig(
     level=logging.INFO,
     filename=log_file_path,
-    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-    handlers=[
-        logging.FileHandler(log_file_path),
-        logging.StreamHandler()
-    ]
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s'
 )
